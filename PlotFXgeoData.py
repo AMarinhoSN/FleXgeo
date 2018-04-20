d2a323d24366c817e87e08f4ae3c9eae8284bc2b
Antonio Marinho <amdnsk@gmail.com>
2018-04-20 19:57:36 +0200

    Violin plots added

--- PlotFXgeoData.py
+++ PlotFXgeoData.py
@@ -11,7 +11,10 @@ print(" USAGE: python PlotXgeoData.py DiffGeo_raw.csv ")
 Csv_filenm = argv[1]
 print("@ Load CSV data of ", Csv_filenm)
 CSVdata = FXgeoToolBox.FXGeoData(Csv_filenm)
-# 2 - Plot curvature and
+# 2 - Plot
 print("@ Plot conformations curvature and torsion per residue")
 CSVdata.plotConfsKTData(showplot=False, REF=None, REFlabel=None, lang="EN",
                         savePDF=False)
+print("@ Generate violin plots of FleXgeo values")
+CSVdata.plotDataViolin(showplot=False, scale="count", inner="stick",
+                       linewidth=.1, bw=.1, lang="EN")
