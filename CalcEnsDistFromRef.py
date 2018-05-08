ed81fb0d8df778ccc40621e804c95177b9cd99c2
Antonio Marinho <amdnsk@gmail.com>
2018-05-08 15:33:03 +0200

    Added input flags to CalcEnsDistFromRef.py

--- CalcEnsDistFromRef.py
+++ CalcEnsDistFromRef.py
@@ -12,12 +12,29 @@ ensemble, but the user can specify another by changing -cref_idx.
 '''
 
 print(" >> CALCulate ENSemble DISTance From a Reference << ")
-print(" USAGE: python CalcEnsDistFromRef.py  ")
+print(" USAGE: python CalcEnsDistFromRef.py  -in=XgeoObjFilname.p [OPTIONS]")
+print(" [OPTIONS]")
+print("        -refidx=[int] : the reference conformation index (default=0)")
+print(" INPUT: ")
+pkl_flnm = None
+refidx = 0
+for arg in argv:
+    if arg == argv[0]:
+        continue
+    if arg.startswith('-in='):
+        pkl_flnm = arg[4:len(arg)]
+        print("   -in = {}".format(pkl_flnm))
+    if arg.startswith("-refidx="):
+        try:
+            refidx = int(arg[8:len(arg)])
+        except(ValueError):
+            print("ERROR: You must provide a valid integer.")
+            exit(1)
+        print("   -refidx = {}".format(refidx))
 
 print("@ Load data")
-pkl_flnm = argv[1]
 XgeoDataObj = pickle.load(open(pkl_flnm, "rb"))
 
 print("@ Calculate KT euclidean distance from reference conformation")
-XgeoDataObj.calcD2ConfperRes(cref_idx=0, showplot=False)
+XgeoDataObj.calcD2ConfperRes(cref_idx=refidx, showplot=False)
 print(":: DONE ::")
